a48b414b2cd8443550f1785a259a364295d1ce32
Kot Andreev <bottlebo@mail.ru>
2018-10-15 13:57:30 +0700

    Change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
   "extends": "airbnb",
-  "plugins": [
-    "react"
-  ],
   "rules": {
+    "semi": "off",
+    "indent": ["error",4],
+    "no-underscore-dangle": "off",
+    "linebreak-style": ["error", "windows"],
     "react/jsx-no-bind": "off",
     "jsx-a11y/href-no-hash": "off"
   },
   "env" : {
     "node": true,
+    "mocha": true,
   }
 };
